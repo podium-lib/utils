34ef48c19109f11f35f0f4f18b5ab0ba0dde79c0
Trygve Lie <trygve.lie@finn.no>
2020-07-14 23:38:44 +0200

    fix: Simplify js and css value validation

--- __tests__/asset-js.js
+++ __tests__/asset-js.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { validate } = require('@podium/schemas');
 const Js = require('../lib/asset-js');
 
 test('Js() - object tag - should be PodiumAssetJs', () => {
@@ -324,5 +325,11 @@ test('Js() - set "src" - should throw', () => {
     });
     expect(() => {
         obj.src = '/bar';
+        console.log('promp')
     }).toThrowError('Cannot set read-only property.');
+});
+
+test('Js() - validate object against schema - should validate', () => {
+    const obj = new Js({ value: '/foo' });
+    expect(validate.js([obj]).error).toEqual(false);
 });
