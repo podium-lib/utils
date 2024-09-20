a07e0e3b69d0a372d20417d9410bd50698c9af19
Richard Walker <digitalsadhu@gmail.com>
2024-09-20 16:34:58 +1200

    fix: receive and emit hinted assets

--- tests/hints.test.js
+++ tests/hints.test.js
@@ -1,15 +1,42 @@
 import tap from 'tap';
 import { Hints } from '../lib/hints.js';
 
-tap.test(
-    'PodiumHttpIncoming() - object tag - should be PodiumHttpIncoming',
-    (t) => {
-        const hints = new Hints();
-        hints.addExpectedHint('foo');
-        hints.addExpectedHint('bar');
-        hints.addReceivedHint('foo');
-        hints.addReceivedHint('bar');
-        t.equal(hints.allHintsReceived, true);
+tap.test('Hints() - allHintsReceived property - should be true', (t) => {
+    const hints = new Hints();
+    hints.addExpectedHint('foo');
+    hints.addExpectedHint('bar');
+    hints.addReceivedHint('foo', {
+        js: [{ value: 'foo.js' }],
+        css: [{ value: 'foo.css' }],
+    });
+    hints.addReceivedHint('bar', {
+        js: [{ value: 'foo.js' }],
+        css: [{ value: 'foo.css' }],
+    });
+    t.equal(hints.allHintsReceived, true);
+    t.end();
+});
+
+tap.test('Hints() - complete event - should fire', (t) => {
+    t.plan(6);
+    const hints = new Hints();
+    hints.on('complete', ({ js, css }) => {
+        t.equal(js.length, 2);
+        t.equal(css.length, 2);
+        t.equal(js[0].value, 'foo.js');
+        t.equal(css[0].value, 'foo.css');
+        t.equal(js[1].value, 'foo.js');
+        t.equal(css[1].value, 'foo.css');
         t.end();
-    },
-);
+    });
+    hints.addExpectedHint('foo');
+    hints.addExpectedHint('bar');
+    hints.addReceivedHint('foo', {
+        js: [{ value: 'foo.js' }],
+        css: [{ value: 'foo.css' }],
+    });
+    hints.addReceivedHint('bar', {
+        js: [{ value: 'foo.js' }],
+        css: [{ value: 'foo.css' }],
+    });
+});
